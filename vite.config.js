dbd0d86d26c9e207b2203a8a7398346686954e5e
Baek Sujin <whitesj121@gmail.com>
2026-01-27 14:04:02 +0900

    경로수정

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // 수진님의 GitHub 레포지토리 이름을 넣으세요.
+  // 예: 레포 이름이 'my-portfolio'라면 '/my-portfolio/'
+  base: '/my-portfolio/', 
+})
